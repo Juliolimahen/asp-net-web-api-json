dda8c8721723cb224b7e5451c9252b1626acb2f1
Juliolimahen <juliolima_henrique@outlook.com>
2022-12-28 23:12:05 -0300

    read json

---
+++ Students/Students/Controllers/StudentController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Students.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Students.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _env;
+
+        public StudentController(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            Student student = new Student(_env);
+            return student.StudentsList();
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public Student Get(int id)
+        {
+            Student student = new Student(_env);
+
+            //return student.StudentsList().Where(x => x.Id == id).FirstOrDefault();
+
+            return student.StudentsList().FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public List<Student> Post(Student student)
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(student);
+
+            return students;
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Students/Students/Model/Student.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Students.Model
+{
+    public class Student
+    {
+        public Student(){}
+
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _env;
+
+        public Student(Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Telephone { get; set; }
+        public string? RA { get; set; }
+
+        public List<Student> StudentsList()
+        {
+            #region
+            //Student student1 = new Student()
+            //{
+            //    Id = 1,
+            //    Name = "Fausto",
+            //    LastName = "Vera",
+            //    Telephone = "11-997737770",
+            //    RA = "0001-22",
+            //};
+
+            //Student student2 = new Student()
+            //{
+            //    Id = 2,
+            //    Name = "Roger",
+            //    LastName = "Guedes",
+            //    Telephone = "11-997737770",
+            //    RA = "0002-22",
+            //};
+
+            //List<Student> studentsList = new List<Student>();
+
+            //studentsList.Add(student1);
+            //studentsList.Add(student2);
+            //return studentsList;
+            #endregion
+
+            // application's base path
+            var contentRootPath = _env.ContentRootPath;
+
+            // application's publishing path
+            var  webRootPath = _env.WebRootPath;
+
+            var docPath = Path.Combine(_env.ContentRootPath, @"App_Data\Base.json");
+
+            var json = File.ReadAllText(docPath);
+            
+            var listStudents = JsonConvert.DeserializeObject<List<Student>>(json);
+
+            return listStudents;
+        }
+    }
+}
